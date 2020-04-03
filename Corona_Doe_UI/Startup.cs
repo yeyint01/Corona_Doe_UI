2ec3a46ca78e9bda88391cc9af01b88a177ddf6e
Thawdar Myint Aung <thawdar@itstar.io>
2020-04-03 12:56:08 +0630

    region form

--- Corona_Doe_UI/Startup.cs
+++ Corona_Doe_UI/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Corona_Doe_UI.Data;
 
 namespace Corona_Doe_UI
 {
@@ -28,7 +27,7 @@ namespace Corona_Doe_UI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            //services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
